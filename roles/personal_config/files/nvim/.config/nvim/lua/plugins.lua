da2bbcd69847316287c9b34b619a00a055a26f67
Kaif Ahmed <dm@kaif.dev>
2021-08-01 00:16:43 +0100

    feat: added nvim-dap

--- roles/personal_config/files/nvim/.config/nvim/lua/plugins.lua
+++ roles/personal_config/files/nvim/.config/nvim/lua/plugins.lua
@@ -2,31 +2,25 @@ return require('packer').startup(function()
     -- core
     use 'wbthomason/packer.nvim'
     use 'neovim/nvim-lspconfig'
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    }
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    use 'mfussenegger/nvim-dap'
 
     -- quality of life
     use 'tpope/vim-vinegar'
     use 'tpope/vim-commentary'
     use 'tpope/vim-fugitive'
     use 'rhlobo/vim-super-retab'
+    use 'hrsh7th/nvim-compe'
 
     -- telescope
     use 'nvim-telescope/telescope.nvim'
-    use 'nvim-lua/popup.nvim'
     use 'nvim-lua/plenary.nvim'
+    use 'nvim-lua/popup.nvim'
     use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
     -- interface
-    use {
-	'folke/zen-mode.nvim',
-	config = function()
-	    require("zen-mode").setup{}
-	end
-    }
-    use 'hrsh7th/nvim-compe'
+    use 'folke/zen-mode.nvim'
+    use 'folke/twilight.nvim'
     use 'hoob3rt/lualine.nvim'
 
     -- linting
